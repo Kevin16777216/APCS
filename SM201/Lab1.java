85db9e4d01e30e7d2dec22c023b99f9e43f8ce02
Kevin16777216 <kcai00@stuy.edu>
2019-02-10 11:14:50 -0500

    add SM2Lab1

--- SM201/Lab1.java
+++ SM201/Lab1.java
@@ -2,19 +2,34 @@ import java.util.Scanner;
 public class Lab1{
     /*
       1.  g(n) = 2nlogn + 3n^2 + n + 1 is O(n^2).
-          n^2 > nlogn > n > 1 in terms of complexity, so it is n^2.
+      n^2 > nlogn > n > 1 in terms of complexity, so it is O(n^2).
       2. |                                                                                             
-	 |                                 
-	 |                                 
-	 4                ..                  
-         3        ........                       
-	 2    ....                            
-	 1  .                                
-	 0 .                                 
-	 X0123456789ABCDEF_
+      |                                 
+      |                                 
+      4                ..                  
+      3        ........                       
+      2    ....                            
+      1  .                                
+      0 .                                 
+      X0123456789ABCDEF_
+      4.
+      T(N) = T(N/2) + 1
+      T(1) = 1;
+	
+      Example:
+      T(2) = T(1) + 1
+      T(4) = T(2) + 1 = T(1) + 1 + 1 = T(1) + log4 = 1 + log4
+      - For every multiple of 2, result increases by 1, so the
+      log base 2 of the input yields the amount of increase.
+      
+      T(N) = 1 + log(N)
+      
+      T(N) = log(N) + 1 is closer log(N) as N goes to infinty, so
+      T(N) is O(log(N)). log(N) is a more complex operation than 1.
+      
      
-*/
-    public static void Main(String[] args){
+    */
+    public static void main(String[] args){
 	int n = Integer.parseInt(args[0]);
 	int N = (int) Math.pow(2,n);
 	System.out.print("Think of a number: ");
@@ -24,12 +39,14 @@ public class Lab1{
     }
     public static int search(int l,int h){
 	Scanner Scan = new Scanner(System.in);
-	if(h == 2)return l;
+	if(h - l == 1){
+	    return l;
+	}
 	int m = l + (h-l)/2;
 	System.out.println("Is it lower than "+m+"?");
 	if(Scan.nextBoolean()){
-	    search(l,m);
+	    return search(l,m);
 	}
-	search(m,h);
+	return search(m,h);
     }
 }
